8b97fe88bc7c917d40fc30192f0f037f552154dc
Sichandu <siddureddy193@gmail.com>
2026-03-24 22:55:14 +0530

    update full app

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
-window.BACKEND_URL = 'http://127.0.0.1:8000';
-// window.BACKEND_URL = 'https://lawyaari.onrender.com';
+// window.BACKEND_URL = 'http://127.0.0.1:8000';
+window.BACKEND_URL = 'https://lawyaari.onrender.com';
